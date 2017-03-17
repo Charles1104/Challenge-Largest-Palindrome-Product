27c0e3578b8a7c30c1b5e50a453bf0c6839e92ce
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-03-17 10:06:20 -1000

    All tests passed and work fo rnay digits

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -9,14 +9,29 @@ module.exports = function(digits){
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
+  var maxDigit = Math.pow(10, digits);
 
-  // do your work here
+  function reverse_a_number(n){
+    n = String(n);
+    return n.split("").reverse().join("");
+  }
 
-
+  for (var i = maxDigit/10; i < maxDigit; i++){
+    for (var j = maxDigit/10; j < maxDigit; j++){
+      if (i * j === Number(reverse_a_number(i * j))){
+        if (palindromeNumber <= i * j){
+          factor_0 = i;
+          factor_1 = j;
+          palindromeNumber = factor_0 * factor_1;
+        }
+      }
+    }
+  }
 
   return {
     factor_0 : factor_0,
     factor_1 : factor_1,
     palindromeNumber : palindromeNumber
   };
+
 };
